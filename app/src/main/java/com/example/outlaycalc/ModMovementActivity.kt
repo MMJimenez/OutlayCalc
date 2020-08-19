cf778e6a1462ba7a0f9b53c3aeab9ffb9ba0ea63
MM Jimenez <munozmiguel321@gmail.com>
2020-08-19 13:06:26 +0200

    added modify fun into modActivity

--- app/src/main/java/com/example/outlaycalc/ModMovementActivity.kt
+++ app/src/main/java/com/example/outlaycalc/ModMovementActivity.kt
@@ -1,15 +1,20 @@
 package com.example.outlaycalc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.outlaycalc.models.Movement
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_add_movement.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_mod_movement.*
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.util.*
 
 class ModMovementActivity : AppCompatActivity() {
 
@@ -34,7 +39,7 @@ class ModMovementActivity : AppCompatActivity() {
         }
     }
 
-    fun getMovement(docId: String) {
+    private fun getMovement(docId: String) {
         val movementToModify =
             db.collection("users").document(auth.currentUser?.uid!!).collection("movements")
                 .document(docId)
@@ -45,7 +50,15 @@ class ModMovementActivity : AppCompatActivity() {
 
                     val movementObject = document.toObject(Movement::class.java)
                     movementObject?.let {
+
                         printMovementData(it)
+
+                        btnModify.setOnClickListener {
+                            setModifiedMovement(movementToModify, movementObject.date)
+                        }
+                        btnDelete.setOnClickListener {
+                            deleteAlertDialog(movementToModify)
+                        }
                         Log.d(TAG, "doc mutable: ${it.description}")
                     }
 
@@ -55,7 +68,7 @@ class ModMovementActivity : AppCompatActivity() {
             }
     }
 
-    fun printMovementData(modMovement: Movement) {
+    private fun printMovementData(modMovement: Movement) {
 
         modInputTxtAmount.setText(modMovement.amount.toString())
         modInputTxtDescription.setText(modMovement.description)
@@ -67,5 +80,41 @@ class ModMovementActivity : AppCompatActivity() {
         }
     }
 
+    private fun setModifiedMovement(docPath: DocumentReference, dateOfMovement: Date) {
+
+        val modMovement = Movement()
+        modMovement.amount = modInputTxtAmount.text.toString().toFloat()
+        modMovement.description = modInputTxtDescription.text.toString()
+        modMovement.date = dateOfMovement
+        if (modRadioIngress.isChecked) {
+            modMovement.outlay = false
+        }
+        docPath.set(modMovement)
+            .addOnCompleteListener {
+                Log.i("miApp", "ha modificado datos correctamente")
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+    }
+
+    fun deleteMovement(docPath: DocumentReference) {
+        docPath.delete()
+            .addOnCompleteListener {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+    }
+
+    fun deleteAlertDialog(docPath: DocumentReference) {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(resources.getString(R.string.title))
+            .setMessage(resources.getString(R.string.supporting_text))
+            .setNeutralButton(resources.getString(R.string.cancel)) { dialog, which ->
+            }
+            .setNegativeButton(resources.getString(R.string.decline)) { dialog, which ->
+            }
+            .setPositiveButton(resources.getString(R.string.accept)) { dialog, which ->
+                deleteMovement(docPath)
+            }
+            .show()
+    }
 
 }
