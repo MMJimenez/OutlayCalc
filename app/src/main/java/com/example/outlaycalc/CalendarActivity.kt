b2219e7395d1278b4f50e2d2ddb5f83cf86a6f8b
MM Jimenez <munozmiguel321@gmail.com>
2020-12-27 20:12:22 +0100

    recyclerCalendar added

--- app/src/main/java/com/example/outlaycalc/CalendarActivity.kt
+++ app/src/main/java/com/example/outlaycalc/CalendarActivity.kt
@@ -1,4 +1,20 @@
 package com.example.outlaycalc
 
-class CalendarActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.util.*
+
+class CalendarActivity : AppCompatActivity() {
+
+    var TAG = "milog"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+
+        var calendar = Calendar.getInstance()
+
+        Log.v(TAG, "$calendar")
+    }
 }
