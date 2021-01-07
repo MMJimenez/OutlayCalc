224be7c8cbc669d3abc9840c5a612d96198ee115
MM Jimenez <munozmiguel321@gmail.com>
2021-01-07 16:43:29 +0100

    update calendar act

--- app/src/main/java/com/example/outlaycalc/CalendarActivity.kt
+++ app/src/main/java/com/example/outlaycalc/CalendarActivity.kt
@@ -3,18 +3,41 @@ package com.example.outlaycalc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import java.time.Month
 import java.util.*
+import java.util.Calendar.*
+import java.util.Locale.ENGLISH
 
 class CalendarActivity : AppCompatActivity() {
 
-    var TAG = "milog"
+    var TAG = "miapp"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
-        var calendar = Calendar.getInstance()
 
-        Log.v(TAG, "$calendar")
+        var today = Calendar.getInstance()
+
+        setDateText(today)
+        getDaysOfTheMonth(today)
+    }
+
+    fun setDateText(today: Calendar) {
+        val monthText = findViewById<TextView>(R.id.monthName)
+
+        val month = today.getDisplayName(MONTH, SHORT, ENGLISH)
+        val year = today.get(YEAR)
+
+        monthText.text = "$month $year"
+        Log.v(TAG, "$month $year")
+    }
+
+    fun getDaysOfTheMonth(today: Calendar) {
+        val firstDayOfWeek = today.getMinimalDaysInFirstWeek()
+//        val lastDayOfWeek = today
+        Log.v(TAG, "$firstDayOfWeek y ")
     }
 }
--- app/src/main/java/com/example/outlaycalc/MainActivity.kt
+++ app/src/main/java/com/example/outlaycalc/MainActivity.kt
@@ -30,6 +30,10 @@ class MainActivity : AppCompatActivity(), CustomItemListener {
 
         topAppBar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
+                R.id.calendar -> {
+                    startActivity(Intent(this, CalendarActivity::class.java))
+                    true
+                }
                 R.id.menuLogOut -> {
                     FirebaseAuth.getInstance().signOut()
                     startActivity(Intent(this, SingInActivity::class.java))
